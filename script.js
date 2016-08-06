de10af907b5de2aa875094178b16b9dc57d4731f
ayab257 <ayab257@gmail.com>
2016-08-06 11:34:43 +0300

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+var person={
+	eyeColor: "brown",
+	firstName: "aya",
+	height: "1.68m",
+	location: "israel,haifa"	
+};
+eyeColor="blue";
+alert(eyeColor);
+person.languages="arabic,hebrew,english";
+typeOf "hebrew";
+alert(typeOf);
